c05d0a2e4677089d05ac9b6e9f5c9cacbbea3dae
ItaloAraujoDev <italoarauju2019@gmail.com>
2023-04-27 18:18:42 -0300

    feat: Adicionando a logo na aba

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -7,7 +7,7 @@ export default function Home() {
                 <div className="w-full h-[90%] absolute top-0 md:top-[-8rem] lg:top-2 xl:top-[-1rem] flex justify-center">
                     <img src={espaco} alt="Imagem do espaço" className='w-full' />
                 </div>
-                <div className="w-[45%] h-80 mt-24 md:mt-40 lg:mt-28 1440:mt-32 2xl:mt-32 full:mt-36 flex flex-col items-center text-center text-white gap-7 md:gap-4">
+                <div data-aos="zoom-in" className="w-[45%] h-80 mt-24 md:mt-40 lg:mt-28 1440:mt-32 2xl:mt-32 full:mt-36 flex flex-col items-center text-center text-white gap-7 md:gap-4">
                     <h3 className="font-bold text-base xl:text-lg 2xl:text-xl full:text-2xl         
                 italic">Bem-vindo à Mesh</h3>
                     <h2 className="text-display-large md:text-3xl lg:text-5xl xl:text-5xl 2xl:text-6xl full:text-7xl leading-custom-leading font-russo-one">
