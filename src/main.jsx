c7ece9632cf585b4ae05d851198d6887045dd7dc
ItaloAraujoDev <italoarauju2019@gmail.com>
2023-05-23 20:21:26 -0300

    retirando o suspense

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Root from './Root';
 import {
@@ -65,8 +65,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Suspense fallback={ <p>Carregando...</p> }>
+
       <RouterProvider router={router} />
-    </Suspense>
+
   </React.StrictMode>,
 )
